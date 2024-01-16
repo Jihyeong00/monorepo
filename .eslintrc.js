232594fd4e31eb78ed54128924ba7e33e24dde94
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2024-01-17 02:38:59 +0900

    :rocket: chore: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    root: true,
+    extends: [],
+    plugins: [],
+    parser: '',
+    rules: {
+    },
+}
---
+++ packages/core/vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths(), dts()],
+  build: {
+    // optional
+    ssr: true,
+    lib: {
+      entry: 'src/index.ts',
+      name: 'index',
+      formats: ['es'],
+    },
+
+    rollupOptions: {
+      external: ['react', 'src/stories/*', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
