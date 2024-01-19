32d8ded71e55051884ce13dbb605b0cc181ccca7
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2024-01-20 00:55:36 +0900

    모노레포 테스트

---
+++ apps/admin/src/app/page.tsx
@@ -0,0 +1,10 @@
+import styles from './page.module.css'
+import {Button} from "@monorepo/share/src/components/Button";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Button/>
+    </main>
+  )
+}
---
+++ packages/share/src/styles/css-token.ts
@@ -0,0 +1,8 @@
+export const cssToken = {
+    color: {
+        primary: '#0f62fe',
+        red: '#ff0000'
+    },
+}
+
+export type cssTokenType = keyof typeof cssToken
