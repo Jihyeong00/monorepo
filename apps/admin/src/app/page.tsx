9ad0d178dd512cbf53b3bfac1b95bf6f731e5c39
Zero <iolpd42@gmail.com>
2024-01-20 02:00:59 +0900

    feat: husky, eslint, prettier 추가하기 완료 (#1)
    * 모노레포 테스트
    
    * test
    
    * test1234
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test

---
+++ apps/admin/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@monorepo/share/src/components/Button";
+
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Button />
+    </main>
+  );
+}
